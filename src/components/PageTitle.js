fd3d6de4beb2880a057c5508faf140a4b40ed596
Andy Gelfo <andyjgelfo@gmail.com>
2023-03-09 11:03:43 -0500

    Login and Register API complete

---
+++ src/components/PageTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PageTitle() //this is JSX - js + html
+{
+   return(
+     <><h1 id="title">COP 4331 MERN Stack Demo</h1><h2>Group 6 - CerealBoxd</h2></>
+
+   );
+};
+
+export default PageTitle; 
