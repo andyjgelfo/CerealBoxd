706790f2ec64026eff521c6ab9f0fbaa46ec3c44
Daisy Nguyen <93001657+dhashira@users.noreply.github.com>
2023-03-19 16:16:09 -0400

    Created navbar, uploaded fonts and logo, set up folders.

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// HOME PAGE 
+
+import React from 'react'; 
+import Navbar from "./Components/Navbar"; 
+
+function App() {
+  return(
+    <React.Fragment>
+      <Navbar/>
+    </React.Fragment>
+  ); 
+}
+
+export default App; 
---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,32 @@
+import { useRef } from "react"; 
+import { FaBars, FaTimes } from "react-icons/fa"; 
+import '../Fonts/SharpGroteskBook20.otf';
+import "../Styles/Navbar.css"
+
+function Navbar() {
+    const navRef = useRef(); 
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav"); 
+    }
+
+    return (
+        <header>
+            <img src={require('../Images/logo.png')} alt='logo' className="logo"/>
+            <nav ref={navRef}>
+                <a href="/#">ABOUT US</a>
+                <a href="/#">CEREALS</a>
+                <a href="/#">LOGIN</a>
+                <a href="/#">REGISTER</a>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes/>
+                </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+            </button>
+        </header>
+    );
+}
+
+export default Navbar; 
