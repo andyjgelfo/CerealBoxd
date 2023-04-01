ba2bb0ae4de2cc236d80944729e3d7148d2c69fe
Daisy Nguyen <93001657+dhashira@users.noreply.github.com>
2023-04-01 15:05:24 -0400

    Putting images instead of names on Cereals page.

--- frontend/src/Components/CerealCard.js
+++ frontend/src/Components/CerealCard.js
@@ -4,7 +4,7 @@ import "../Styles/CerealCard.css";
 const CerealCard = ({ cerealData }) => {
     return (
         <div className="card">
-            <div className="card_title">{cerealData.image}</div>
+            <div className="card_title"><img src={cerealData.image}/></div>
         </div>
     ); 
 }; 
