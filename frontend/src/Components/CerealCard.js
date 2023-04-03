0bed3f589a1fbbf6fd897c1674c9262f1a13bca9
Daisy Nguyen <93001657+dhashira@users.noreply.github.com>
2023-04-03 10:58:03 -0400

    Making cereals clickable on Cereals page.

--- frontend/src/Components/CerealCard.js
+++ frontend/src/Components/CerealCard.js
@@ -1,14 +1,13 @@
 import React from 'react'; 
 import "../Styles/CerealCard.css";
-import { Link } from 'react-router-dom'; 
 
 const CerealCard = ({ cerealData }) => {
     return (
-        <Link to={'/InfoPage'}>
+        <a href="AboutPage">
             <div className="card">
             <img src={cerealData.image}/>
             </div>
-        </Link>
+        </a>
     ); 
 }; 
 
