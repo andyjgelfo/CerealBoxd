3f4e399ea70205d089c49a195ccc78f356dd76c0
Daisy Nguyen <93001657+dhashira@users.noreply.github.com>
2023-04-15 02:50:01 -0400

    Minor adjustments.

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -13,14 +13,14 @@ function Home()
     }, []);
 
     return(
-        <div>
+        <div> 
             <div class = "BannerNButton">
                 <img src={require('../Images/BannerFade2Text.png')} alt='banner' className="banner"/>
                 <a href="LoginPage">
-                    <button class = "logBut">Login</button>
+                    <button class = "logBut">LOGIN</button>
                 </a>
                 <a href="RegisterPage">
-                    <button class = "regBut">Register</button>
+                    <button class = "regBut">REGISTER</button>
                 </a>
             </div>
             <h1 data-aos = "fade-up">Featured Articles</h1>
@@ -33,13 +33,9 @@ function Home()
                             <img src={require('../Images/SadCereal.png')} className="cards" />
                         </figure>
                         <div class="article-body">
-                            <h2>&nbsp;Top 10 Cereals For A Rainy Day</h2>
-                            <p>
-                            &nbsp;Enter Description Here.
-                            </p>
-                            <a href="#" class="read-more">
-                            &nbsp;Read more <span class="sr-only">...</span>
-                            </a>
+                            <h2>Top 10 Cereals For A Rainy Day</h2>
+                            <p>Enter Description Here.</p>
+                            <center><a href="#" class="read-more">Read More</a></center>
                         </div>
                     </div>
                 </article>
@@ -50,13 +46,9 @@ function Home()
                             <img src={require('../Images/RaisinBran.png')} className="cards" />
                         </figure>
                         <div class="article-body">
-                            <h2>&nbsp;Editor's Choice: Raisin Bran</h2>
-                            <p>
-                            &nbsp;Enter some description here.
-                            </p>
-                            <a href="#" class="read-more">
-                            &nbsp;Read more <span class="sr-only">...</span>
-                            </a>
+                            <h2>Editor's Choice: Raisin Bran</h2>
+                            <p>Enter Description Here.</p>
+                            <center><a href="#" class="read-more">Read More</a></center>
                         </div>
                     </div>
                 </article>
@@ -67,26 +59,16 @@ function Home()
                             <img src={require('../Images/SugarFree.png')} className="cards" />
                         </figure>
                         <div class="article-body">
-                            <h2>&nbsp;Top 5 Low-Sugar Cereals</h2>
-                            <p>
-                            &nbsp;Some description here!!
-                            </p>
-                            <a href="#" class="read-more">
-                            &nbsp;Read more <span class="sr-only">...</span>
-                            </a>
+                            <h2>Top 5 Low-Sugar Cereals</h2>
+                            <p>Enter Description Here.</p>
+                            <center><a href="#" class="read-more">Read More</a></center>
                         </div>
                     </div>
                 </article>
             </section>
         </div>
-           
-        
     );
-
 };
-
-
-
 
 export default Home;
 
