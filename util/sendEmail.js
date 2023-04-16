fdb37d0717cb500416961350d985c92020588098
AppleThomas <thomasnguyen3212@gmail.com>
2023-04-16 13:07:02 -0400

    testing with heroku

--- util/sendEmail.js
+++ util/sendEmail.js
@@ -1,24 +1,36 @@
-require("dotenv").config();
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// require("dotenv").config();
+// // const sgMail = require("@sendgrid/mail");
+// // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// var nodemailer = require('nodemailer');
 
-const sendEmail = (to, from, subject, text) => {
-  const msg = {
-    to: to,
-    from: from,
-    subject: subject,
-    text: text
-  };
+// const sendEmail = (to, from, subject, text) => {
+//   const msg = {
+//     to: to,
+//     from: from,
+//     subject: subject,
+//     text: text
+//   };
 
-  sgMail.send(msg, function (err, result) {
-    if (err) {
+// //   sgMail.send(msg, function (err, result) {
+// //     if (err) {
         
-      console.log("email faileed")
-      console.log(err);
-    } else {
-      console.log("Email was Sent");
-    }
-  });
-};
+// //       console.log("email faileed")
+// //       console.log(err);
+// //     } else {
+// //       console.log("Email was Sent");
+// //     }
+// //   });
 
-module.exports = sendEmail;
+// // create reusable transporter object using the default SMTP transport
+// transport = nodemailer.createTransport({
+//   host: "smtp.ethereal.email",
+//   port: 587,
+//   secure: false, // true for 465, false for other ports
+//   auth: {
+//     user: testAccount.user, // generated ethereal user
+//     pass: testAccount.pass, // generated ethereal password
+//   },
+// });
+// };
+
+// module.exports = sendEmail;
