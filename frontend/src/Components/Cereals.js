b10934c7eb9965b628dc6b6b2e54fa4d14ed759a
Daisy Nguyen <93001657+dhashira@users.noreply.github.com>
2023-04-18 11:53:13 -0400

    Added modal and FAB for suggest cereal feature, styling adjustments.

--- frontend/src/Components/Cereals.js
+++ frontend/src/Components/Cereals.js
@@ -3,9 +3,13 @@ import "../Styles/Cereals.css";
 import { useState, useEffect } from 'react'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom"; 
-// import { HiPlusCircle } from "react-icons/hi"; 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Modal from 'react-modal'; 
+import { MdNotifications } from "react-icons/md"; 
+import { BsFillBrightnessAltHighFill } from "react-icons/bs";
+import { FaTimes } from "react-icons/fa"; 
+import { Container, Button} from 'react-floating-action-button'; 
 
 function Cereals()
 {
@@ -70,26 +74,124 @@ function Cereals()
     if (JSON.stringify(tokenResponse) === "null")
         sillyMessage = "Silly rabbit, you're not signed in!"
     else
-        sillyMessage = "Silly rabbit, Cerealboxd is for " + tokenResponse.username + "!";
+        sillyMessage = "Silly rabbit, Cerealboxd is for @" + tokenResponse.username + "!";
 
+    
+    // Modal: Pop-up message for requesting a new cereal
+    const [modalIsOpen, setModalIsOpen] = useState(false); 
+    const setModalIsOpenToTrue = () => {
+        setModalIsOpen(true); 
+        document.body.style.overflow = 'hidden';
+
+    }
+    const setModalIsOpenToFalse = () => {
+        setModalIsOpen(false); 
+        document.body.style.overflow = 'unset';
+    }
+
+    const customStyles1 = {
+        content : {
+          top: '52%',
+          left: '50%',
+          right: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          backgroundColor:'#F2EAC1' , 
+          borderStyle: 'solid', 
+          borderColor: '#1C2143',
+          borderWidth: '0.6em',
+          borderRadius: '32px',
+          width: '50em',
+          height: 'max-content',
+          display: 'flex', 
+          flexWrap: 'wrap', 
+          alignItems: 'center', 
+          justifyItems: 'center', 
+          flexDirection: 'column',
+          paddingLeft: '4em', 
+          paddingRight: '4em',
+          paddingTop: '4em', 
+          paddingBottom: '4em',
+          textAlign: 'center', 
+        }
+    };
+
+    const customStyles2 = {
+        content : {
+          top: '28%',
+          left: '50%',
+          right: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          backgroundColor:'#F2EAC1' , 
+          borderStyle: 'solid', 
+          borderColor: '#1C2143',
+          borderWidth: '0.6em',
+          borderRadius: '32px',
+          width: '50em',
+          height: 'max-content',
+          display: 'flex', 
+          flexWrap: 'wrap', 
+          alignItems: 'center', 
+          justifyItems: 'center', 
+          flexDirection: 'column',
+          paddingLeft: '4em', 
+          paddingRight: '4em',
+          paddingTop: '4em', 
+          paddingBottom: '4em',
+          textAlign: 'center',  
+        }
+    };
 
     return(
         <div className='container d-flex align-items-center justify-content-center' id="wrapper">
+            {/* Title */}
             <div className="title">
                 {sillyMessage}
             </div>
 
+            {/* Search bar */}
             <div className="search" data-aos="fade">
-                 <input type="text" id="searchBar" placeholder="SEARCH CEREALS..." onInput={filterCards} />
-                {/* <button id="newCerealButton" onClick={handleClick}>
-                    <HiPlusCircle />
-                </button> */}
+                <input type="text" id="searchBar" placeholder="SEARCH..." onInput={filterCards} />
             </div>
 
-            {/* <div style={{display: isShown ? 'block' : 'none'}}>
-                <div>Some content here</div>
-            </div> */}
+            {/* Suggest new cereal button  */}
+            <Container styles={{background:'none', border: 'none', left: '92%', top: '85%',}}>
+                <Button styles={{backgroundColor: '#F2EAC1', color:'#1C2143', fontSize: '2.4em', textAlign: 'center', paddingTop: '0.1em', position:'BottomRight'}} onClick={setModalIsOpenToTrue}>+</Button>
+            </Container>
 
+            {/* Logged in  */}
+            {localStorage.getItem('user_data') && (<Modal style={customStyles1} isOpen={modalIsOpen} onRequestClose={setModalIsOpenToFalse}>
+                    <center><span id="modalTitle"><BsFillBrightnessAltHighFill id="modalIcon"/> Suggest A New Cereal <BsFillBrightnessAltHighFill id="modalIcon"/></span></center>
+                    <center><span id="modalSubtitle">Don't see your favorite cereal? Thinking outside of the (cereal) box?</span></center>
+                    <center><span id="modalSubtitle">Send in the details below and we'll do our research!</span></center>
+                    <br />
+                    <br />
+                    <center><input type="text" id="suggestCerealName" placeholder='CEREAL NAME'/><br /></center>
+                    <br />
+                    <center><input type="text" id="suggestManufacturer" placeholder='MANUFACTURER'/><br /></center>
+                    <br />
+                    <br />
+                    <center><button id="modalSubmit">SUBMIT</button></center>
+                    <button id="modalClose" onClick={setModalIsOpenToFalse}>
+                        <FaTimes id="modalCloseIcon" />
+                    </button>
+                    <br />
+                    <br />
+                    {/* <center><span id="modalSubmitSuccess">Submitted successfully!</span></center> */}
+            </Modal>)}
+
+            {/* Logged out */}
+            {!localStorage.getItem('user_data') && (<Modal style={customStyles2} isOpen={modalIsOpen} onRequestClose={setModalIsOpenToFalse}>
+                    <center><span id="modalTitle"><MdNotifications id="modalIcon"/> No Milk Before Cereal <MdNotifications id="modalIcon"/></span></center>
+                    <br />
+                    <center><span id="modalSubtitle"><a id="login" href="/LoginPage">Login</a> or <a id="register" href="/RegisterPage">register</a> for an account in order to suggest a cereal to be added!</span></center>
+                    <button id="modalClose" onClick={setModalIsOpenToFalse}>
+                        <FaTimes id="modalCloseIcon" />
+                    </button>
+            </Modal>)}
+
+            {/* Displays cereal images  */}
             <div id="cardContainer">
                 {cereal.map(cereal2 => {
                     return (
@@ -109,6 +211,10 @@ function Cereals()
 }
 
 export default Cereals;
+
+
+
+
 
   
 
