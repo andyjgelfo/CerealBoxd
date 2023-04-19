29c5456e30ae68964e2e9310092637be295c49be
Daisy Nguyen <93001657+dhashira@users.noreply.github.com>
2023-04-19 02:30:07 -0400

    Set up the Reviews area on the cereal card.

--- frontend/src/Components/CerealCard.js
+++ frontend/src/Components/CerealCard.js
@@ -8,6 +8,7 @@ import { MdMenuBook } from "react-icons/md";
 import { BsSpeedometer2 } from "react-icons/bs"; 
 import { BsStars } from "react-icons/bs"; 
 import { IoMdNutrition } from "react-icons/io"; 
+import { RiStarSmileLine } from "react-icons/ri"; 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -65,86 +66,110 @@ const CerealCard = (_) => {
 
     const [rating, setRating] = useState(null); 
     const [hover, setHover] = useState(null);  
+    const [reviewRating, setReviewRating] = useState(null);
 
+    const handleReviewRating = (event) => {
+      setReviewRating(event.target.value);
+   
+    };
+   
     return(
         <div className='container d-flex align-items-center justify-content-center' id="wrapperCerealCard">
-            <div id="imageContainer" data-aos="flip-right">
-                <img id="image" src={info.image}></img> 
-                <div id="likeArea">
-                    <span id="likeLabel">Like</span>
-                    <button id="likeButton">
-                        <VscHeartFilled />
-                    </button>   
-                </div>    
+                {/* Image, Like Button, Rate Buttons */}
+                <div id="imageContainer" data-aos="flip-right">
+                    <img id="image" src={info.image}></img> 
+                    <div id="likeArea">
+                        <span id="likeLabel">Like</span>
+                        <button id="likeButton">
+                            <VscHeartFilled />
+                        </button>   
+                    </div>    
 
-                <span id="rateLabel">Rate</span>
-                <div id="rateArea">
-                    {[...Array(5)].map((star,i) => {
-                        const ratingValue = i + 1; 
-                        return (
-                            <label>
-                                <input
-                                    type="radio"
-                                    name="rating"
-                                    value={ratingValue}
-                                    onClick={() => setRating(ratingValue)}
-                                />
-                                <BsFillStarFill
-                                    className="star"
-                                    color={ratingValue <= (hover || rating) ? "#40bcf4" : "#ffffff"}
-                                    onMouseEnter={() => setHover(ratingValue)}
-                                    onMouseLeave={() => setHover(null)}
-                                />
-                            </label>
-                        ); 
-                    })}
-                </div>   
-            </div>
-
-            <div id="infoContainer" data-aos="fade">
-                <span id="name">{info.name}</span>
-                <span id="subtitle">Produced by {info.manufacturer}</span>
-                <span id="description">{info.description}</span>
-                <span id="ratingLabel"><BsStars id="ratingIcon"/> Overall Rating <BsStars id="ratingIcon"/></span>
-                <span id="rating">{info.rating} / 5</span>
-                <span id="ingredientsLabel"><MdMenuBook id="ingredientsIcon"/> Ingredients <MdMenuBook id="ingredientsIcon"/></span>
-                <span id="ingredients">{info.ingredients}</span>
-
-                <span id="nutritionLabel"><IoMdNutrition id="nutritionIcon"/> Nutrition Facts <IoMdNutrition id="nutritionIcon"/></span>
-                <span class="nutritionFacts">Serving Size: {nutrition.servingSize} g</span>
-                <span class="nutritionFacts">Calories: {nutrition.calories}</span>
-                <span class="nutritionFacts">Total Fat: {nutrition.totalFat} g</span>
-                <span class="nutritionFacts">Saturated Fat: {nutrition.saturatedFat} g</span>
-                <span class="nutritionFacts">Trans Fat: {nutrition.transFat} g</span>
-                <span class="nutritionFacts">Polyunsaturated Fat: {nutrition.polyunsaturatedFat} g</span>
-                <span class="nutritionFacts">Monounsaturated Fat: {nutrition.monounsaturatedFat} g</span>
-                <span class="nutritionFacts">Cholesterol: {nutrition.cholesterol} mg</span>
-                <span class="nutritionFacts">Sodium: {nutrition.sodium} mg</span>
-                <span class="nutritionFacts">Total Carbohydrate: {nutrition.totalCarbohydrate} g</span>
-                <span class="nutritionFacts">Dietary Fiber: {nutrition.dietaryFiber} g</span>
-                <span class="nutritionFacts">Total Sugars: {nutrition.totalSugars} g</span>
-                <span class="nutritionFacts">Added Sugars: {nutrition.addedSugars} g</span>
-                <span class="nutritionFacts">Protein: {nutrition.protein} g</span>
-                <span class="nutritionFacts">Vitamin D: {(nutrition.vitaminD * 100)}%</span>
-                <span class="nutritionFacts">Calcium: {(nutrition.calcium* 100)}%</span>
-                <span class="nutritionFacts">Iron: {(nutrition.iron * 100)}%</span>
-                <span class="nutritionFacts">Potassium: {(nutrition.potassium* 100)}%</span>
-                <span class="nutritionFacts">Vitamin C: {(nutrition.vitaminC* 100)}%</span>
-                <span class="nutritionFacts">Thiamin: {(nutrition.thiamin* 100)}%</span>
-                <span class="nutritionFacts">Riboflavin: {(nutrition.riboflavin* 100)}%</span>
-                <span class="nutritionFacts">Niacin: {(nutrition.niacin* 100)}%</span>
-                <span class="nutritionFacts">Phosphorus: {(nutrition.phosphorus* 100)}%</span>
-                <span class="nutritionFacts">Magnesium: {(nutrition.magnesium* 100)}%</span>
-                <span class="nutritionFacts">Zinc: {(nutrition.zinc* 100)}%</span>
-                <span class="nutritionFacts">Selenium: {(nutrition.selenium* 100)}%</span>
-                <span class="nutritionFacts">Copper: {(nutrition.copper* 100)}%</span>
-                <span class="nutritionFacts">Manganese: {(nutrition.manganese* 100)}%</span>
-                <span class="nutritionFacts">Vitamin B6: {(nutrition.vitaminB6* 100)}%</span>
-                <span class="nutritionFacts">Folate: {(nutrition.folate* 100)}%</span>
-                <span class="nutritionFacts">Vitamin B12: {(nutrition.vitaminB12* 100)}%</span>
-
-                <span id="willItKillYouLabel"><BsSpeedometer2 id="killIcon"/> Will It Kill You Meter <BsSpeedometer2 id="killIcon"/></span>
-                <span id="willItKillYou">{info.willItKillYou}</span>
+                    <span id="rateLabel">Rate</span>
+                    <div id="rateArea">
+                        {[...Array(5)].map((star,i) => {
+                            const ratingValue = i + 1; 
+                            return (
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="rating"
+                                        value={ratingValue}
+                                        onClick={() => setRating(ratingValue)}
+                                    />
+                                    <BsFillStarFill
+                                        className="star"
+                                        color={ratingValue <= (hover || rating) ? "#40bcf4" : "#ffffff"}
+                                        onMouseEnter={() => setHover(ratingValue)}
+                                        onMouseLeave={() => setHover(null)}
+                                    />
+                                </label>
+                            ); 
+                        })}
+                    </div>  
+                </div>
+                
+                {/* Information  */}
+                <div id="infoContainer" data-aos="fade">
+                    <span id="name">{info.name}</span>
+                    <span id="subtitle">Produced by {info.manufacturer}</span>
+                    <span id="description">{info.description}</span>
+                    <span id="ratingLabel"><BsStars id="ratingIcon"/> Overall Rating <BsStars id="ratingIcon"/></span>
+                    <span id="rating">{info.rating} / 5</span>
+                    <span id="ingredientsLabel"><MdMenuBook id="ingredientsIcon"/> Ingredients <MdMenuBook id="ingredientsIcon"/></span>
+                    <span id="ingredients">{info.ingredients}</span>
+                    <span id="nutritionLabel"><IoMdNutrition id="nutritionIcon"/> Nutrition Facts <IoMdNutrition id="nutritionIcon"/></span>
+                    <span class="nutritionFacts">Serving Size: {nutrition.servingSize} g</span>
+                    <span class="nutritionFacts">Calories: {nutrition.calories}</span>
+                    <span class="nutritionFacts">Total Fat: {nutrition.totalFat} g</span>
+                    <span class="nutritionFacts">Saturated Fat: {nutrition.saturatedFat} g</span>
+                    <span class="nutritionFacts">Trans Fat: {nutrition.transFat} g</span>
+                    <span class="nutritionFacts">Polyunsaturated Fat: {nutrition.polyunsaturatedFat} g</span>
+                    <span class="nutritionFacts">Monounsaturated Fat: {nutrition.monounsaturatedFat} g</span>
+                    <span class="nutritionFacts">Cholesterol: {nutrition.cholesterol} mg</span>
+                    <span class="nutritionFacts">Sodium: {nutrition.sodium} mg</span>
+                    <span class="nutritionFacts">Total Carbohydrate: {nutrition.totalCarbohydrate} g</span>
+                    <span class="nutritionFacts">Dietary Fiber: {nutrition.dietaryFiber} g</span>
+                    <span class="nutritionFacts">Total Sugars: {nutrition.totalSugars} g</span>
+                    <span class="nutritionFacts">Added Sugars: {nutrition.addedSugars} g</span>
+                    <span class="nutritionFacts">Protein: {nutrition.protein} g</span>
+                    <span class="nutritionFacts">Vitamin D: {(nutrition.vitaminD * 100)}%</span>
+                    <span class="nutritionFacts">Calcium: {(nutrition.calcium* 100)}%</span>
+                    <span class="nutritionFacts">Iron: {(nutrition.iron * 100)}%</span>
+                    <span class="nutritionFacts">Potassium: {(nutrition.potassium* 100)}%</span>
+                    <span class="nutritionFacts">Vitamin C: {(nutrition.vitaminC* 100)}%</span>
+                    <span class="nutritionFacts">Thiamin: {(nutrition.thiamin* 100)}%</span>
+                    <span class="nutritionFacts">Riboflavin: {(nutrition.riboflavin* 100)}%</span>
+                    <span class="nutritionFacts">Niacin: {(nutrition.niacin* 100)}%</span>
+                    <span class="nutritionFacts">Phosphorus: {(nutrition.phosphorus* 100)}%</span>
+                    <span class="nutritionFacts">Magnesium: {(nutrition.magnesium* 100)}%</span>
+                    <span class="nutritionFacts">Zinc: {(nutrition.zinc* 100)}%</span>
+                    <span class="nutritionFacts">Selenium: {(nutrition.selenium* 100)}%</span>
+                    <span class="nutritionFacts">Copper: {(nutrition.copper* 100)}%</span>
+                    <span class="nutritionFacts">Manganese: {(nutrition.manganese* 100)}%</span>
+                    <span class="nutritionFacts">Vitamin B6: {(nutrition.vitaminB6* 100)}%</span>
+                    <span class="nutritionFacts">Folate: {(nutrition.folate* 100)}%</span>
+                    <span class="nutritionFacts">Vitamin B12: {(nutrition.vitaminB12* 100)}%</span>
+                    <span id="willItKillYouLabel"><BsSpeedometer2 id="killIcon"/> Will It Kill You Meter <BsSpeedometer2 id="killIcon"/></span>
+                    <span id="willItKillYou">{info.willItKillYou}</span>
+                </div>
+            
+            {/* Reviews Area  */}
+            <div id="reviewsContainer" className='container d-flex align-items-center justify-content-center'>
+                <span id="reviewsTitle"><RiStarSmileLine id="reviewsIcon"/> REVIEWS <RiStarSmileLine id="reviewsIcon"/></span>
+                <br />
+                <select id="reviewsDropDown" onChange={handleReviewRating}>
+                    <option>Rating Given?</option>
+                    <option value="oneStar">1 Star</option>
+                    <option value="twoStars">2 Stars</option>
+                    <option value="threeStars">3 Stars</option>
+                    <option value="fourStars">4 Stars</option>
+                    <option value="fiveStars">5 Stars</option>
+                </select>
+                <br/>
+                <textarea className='container d-flex align-items-center justify-content-center' placeholder="Add Review..." id="reviewsBody" rows={4} cols={60} />
+                <br />
+                <button id="reviewsButton">ADD</button>
             </div>
         </div>
     ); 
